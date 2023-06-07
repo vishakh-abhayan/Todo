322b5c006e865431e1a3f0a946a9dbcb08717292
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-06-07 12:58:53 +0530

    singup page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,29 @@
-import "./Login.css";
+import "./Auth.css";
+import { BsPersonFillCheck } from "react-icons/bs";
 
 function Login() {
   return (
     <div className="main_div">
       <div className="log_board">
-        <h1>Login</h1>
+        <h1>
+          <BsPersonFillCheck size={50} />
+          Login
+        </h1>
         <div className="input_field">
-          <input placeholder="Username" type="email" name="email" id="email" />
+          <input
+            placeholder="username"
+            type="username"
+            name="username"
+            id="username"
+            autoComplete="off"
+          />
           {/* <input placeholder="@email" type="email" name="email" id="email" /> */}
-          <input placeholder="password" type="email" name="email" id="email" />
+          <input
+            placeholder="password"
+            type="password"
+            name="password"
+            id="password"
+          />
           <div className="log_buttons">
             <button className="button_log">Get Start</button>
             <button className="button_sing">create an account</button>
--- src/pages/Singup.tsx
+++ src/pages/Singup.tsx
@@ -1,5 +1,44 @@
+import { BsArrowLeft, BsPersonFillAdd } from "react-icons/bs";
+
 function Singup() {
-  return <h1>hello</h1>;
+  return (
+    <div className="main_div">
+      <div className="log_board">
+        <h1>
+          <BsPersonFillAdd size={50} />
+          SingUp
+        </h1>
+        <div className="input_field">
+          <input
+            placeholder="username"
+            type="username"
+            name="username"
+            id="username"
+            autoComplete="off"
+          />
+          <input
+            placeholder="email"
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="off"
+          />
+          <input
+            placeholder="password"
+            type="password"
+            name="password"
+            id="password"
+          />
+          <div className="log_buttons">
+            <button className="button_log">
+              <BsArrowLeft /> back to login
+            </button>
+            <button className="button_sing"> Sing up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Singup;
